6e8c4bf33f718665667eeee890c971336d9fc1c1
Mio Yasutake <mio.fujita.01@gmail.com>
2022-03-09 08:24:10 +0900

    add approuter

---
+++ app/miyasuta.transportui/src/controller/App.controller.ts
@@ -0,0 +1,13 @@
+import BaseController from "./BaseController";
+
+/**
+ * @namespace miyasuta.transportui.controller
+ */
+export default class App extends BaseController {
+
+	public onInit() : void {
+		// apply content density mode to root view
+		this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
+	}
+
+}
